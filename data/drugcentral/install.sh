c2c8d470c741b03dc9a004ad535f8f9d59105dc0
krassowski <krassowski.michal@gmail.com>
2019-01-04 14:40:33 +0000

    Improve install instructions for DrugCentral

--- data/drugcentral/install.sh
+++ data/drugcentral/install.sh
@@ -1,9 +1,13 @@
 sudo su - postgres -c 'createdb -T template0 drugcentral'
 sudo su - postgres -c "gunzip -c $(pwd)/drugcentral.dump.08262018.sql.gz | psql drugcentral"
 sudo su - postgres -c 'createuser drugcentral -w'
-sudo -u postgres psql
+cat << END
+Follow this template to setup the user and privilages:
+
 grant all privileges on database drugcentral to drugcentral;
-alter user drugcentral with encrypted password password;
+alter user drugcentral with encrypted password "password";
 \c drugcentral
 GRANT USAGE ON SCHEMA public TO drugcentral;
 GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO drugcentral;
+END
+sudo -u postgres psql
