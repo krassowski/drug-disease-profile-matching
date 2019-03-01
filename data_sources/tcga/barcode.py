6dea2dfc275d56af3d49799d3a00d5c8fd8651b4
krassowski <krassowski.michal@gmail.com>
2019-03-01 18:34:07 +0000

    Enable comparison of TCGABarcodes

--- data_sources/tcga/barcode.py
+++ data_sources/tcga/barcode.py
@@ -141,3 +141,6 @@ class TCGABarcode(Barcode):
 
     def __repr__(self):
         return f'<TCGA Barcode {self.barcode}>'
+    
+    def __eq__(self, other):
+        return self.barcode == other.barcode
